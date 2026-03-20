f84d84558d2c30272e78800d05d38284e37596aa
Davi Soares Coelho Gomes <davi.gomes2@estudante.ifto.edu.br>
2026-03-19 21:52:02 -0300

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,6 @@ use App\Jobs\EmailJob;
 use App\Mail\WelcomeEmail;
 use App\Models\Products;
 use App\Models\User;
-use Faker\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
